5eba9c8a2631b1002d7bf28c9cce7ad20fce1d29
Lukas <lukaskrepel@gmail.com>
2024-05-24 16:27:36 +0200

    Update random_seq.py

--- random_seq.py
+++ random_seq.py
@@ -6,12 +6,17 @@ from pymiere.wrappers import move_clip
 from pymiere.wrappers import time_from_seconds
 import random
 import time
+import re
 
 # Constants
 ALLOWED_REPETITIVE_COUNT = 3
-CLIP_FOLDER = "VOORCOMP_MP4"
-AUDIO_FOLDER = "AUDIO"
-SKETCH_MUSIC = "Sketches_Score_EndlessLoop.wav"
+CLIP_FOLDER_NAME = "VOORCOMP_MP4"
+AUDIO_FOLDER_NAME = "AUDIO"
+SKETCH_MUSIC_ITEM_NAME = "Sketches_Score_EndlessLoop.wav"
+INTRO_ITEM_NAME = "IntroMetDinos.mp4"
+OUTRO_ITEM_NAME = "OUTRO"
+BUMPERS_FOLDER_NAME = "BUMPERS & WIPES"
+SEQUENCES_FOLDER_NAME = "PREM_SEQS"
 
 # premiere uses ticks as its base time unit, this is used to convert from ticks to seconds
 TICKS_PER_SECONDS = 254016000000
@@ -20,12 +25,19 @@ TICKS_PER_SECONDS = 254016000000
 CATEGORY_WEIGHTS = {
 	'DinoFacts': 1,
 	'Sketches': 0.6,
+	'MagicShow': 0.6,
 	'Construction': 0.5,
 	'DinoDayCare': 0.4,
 	'BabyMatch': 0.3,
 	'Bones': 0.2,
 	'ABC': 0.2,
-	'WrongHeads': 1
+	'MysteryAnimals': 0.2,
+	'BarnDoors': 0.2,
+	'WrongHeads': 0.1,
+	'Drawing and Coloring': 0.1,
+	'Memory': 0.1,
+	'Puzzle': 0.1,
+	'Counting': 0.1
 }
 DEFAULT_WEIGHT = 0.1
 
@@ -34,8 +46,13 @@ def main():
 	print("---START---")
 	project = pymiere.objects.app.project
 	qe_project = pymiere.objects.qe.project
+	first_video_code = extract_first_video_code(project.name)
 	videos = get_videos(project)
-	playlist = create_video_playlist(videos)
+	playlist = create_video_playlist(videos, first_video_code)
+	intro_item = find_item_in_folder(project, BUMPERS_FOLDER_NAME, INTRO_ITEM_NAME)
+	playlist = add_to_playlist(playlist, intro_item, 3)
+	outro_item = find_item_in_folder(project, BUMPERS_FOLDER_NAME, OUTRO_ITEM_NAME)
+	playlist = add_to_playlist(playlist, outro_item, len(playlist))
 	create_sequence(project, playlist)
 	move_sketches_audio_down_a_layer(project.activeSequence)
 	remove_temp_video_track(qe_project)
@@ -43,6 +60,12 @@ def main():
 	add_transitions(qe_project)
 	remove_empty_tracks(qe_project)
 	print("---FINISHED---")
+
+def add_to_playlist(playlist, item, pos):
+	duration = (item.getOutPoint(1).seconds - item.getInPoint(1).seconds)
+	intro = Video(item.name, duration, "Intro", item) #???
+	playlist.insert(pos, intro)
+	return playlist
 
 # Video class to store video metadata
 class Video:
@@ -54,15 +77,41 @@ class Video:
 	def __repr__(self):
 		return f"{self.filename} ({time.strftime('%H:%M:%S', time.gmtime(self.duration))} min, {self.category})"
 
-def get_music(project):
-	print("Getting music...") #TODO fix this code cleanup
+def extract_first_video_code(filename):
+	# Define the regular expression pattern
+	pattern = r'_([^_]+)_v\d+_.+\..+$'
+	# Search for the pattern in the filename
+	match = re.search(pattern, filename)
+	# If a match is found, return the captured group
+	if match:
+		first_video_code = match.group(1)
+		print("First Video Code: " + first_video_code)
+		return first_video_code
+	else:
+		return "no code found"
+
+def find_item_in_folder(project, folder_name, file_name):
+	print("Getting file...")
 	for i in range(project.rootItem.children.numItems):
 		item = project.rootItem.children[i]
-		if item.name == AUDIO_FOLDER:
+		if item.name == folder_name:
 			for j in range(item.children.numItems):
 				child_item = item.children[j]
-				if child_item.name == SKETCH_MUSIC:
+				if child_item.name == file_name:
 					return child_item
+	return None
+
+def find_item(item, item_name):
+	#print(f"Searching item {item_name}...")
+	if item.name == item_name:
+		return item
+	#print(item.type)
+	if item.type == 2 or item.type == 3: # (2 = BIN, 3 = ROOTITEM)
+		for i in range(item.children.numItems):
+			found_item = find_item(item.children[i], item_name)
+			if found_item:
+				return found_item
+	return None
 
 # Function to get all videos from the project
 def get_videos(project):
@@ -71,7 +120,7 @@ def get_videos(project):
 	categories = set()
 	for i in range(project.rootItem.children.numItems):
 		item = project.rootItem.children[i]
-		if item.name == CLIP_FOLDER:
+		if item.name == CLIP_FOLDER_NAME:
 			for j in range(item.children.numItems):
 				childItem = item.children[j]
 				category = childItem.name
@@ -87,7 +136,7 @@ def get_videos(project):
 
 
 # Function to create a video playlist with weighted random selection
-def create_video_playlist(videos, target_duration=60*60):
+def create_video_playlist(videos, first_video_code="", target_duration=60*60):
 	print("Creating playlist...")
 	playlist = []
 	total_duration = 0
@@ -107,7 +156,10 @@ def create_video_playlist(videos, target_duration=60*60):
 		if not valid_videos:
 			break
 		# Get the weights for the valid videos, using the default weight if the category is not defined
-		weights = [CATEGORY_WEIGHTS.get(video.category, DEFAULT_WEIGHT) for video in valid_videos]
+		weights = [
+			CATEGORY_WEIGHTS.get(video.category, DEFAULT_WEIGHT) + (99999 if first_video_code in video.filename else 0)
+			for video in valid_videos
+		]
 		next_video = random.choices(valid_videos, weights=weights, k=1)[0]
 		playlist.append(next_video)
 		videos.remove(next_video)
@@ -131,6 +183,18 @@ def create_sequence(project, playlist):
 		arrayOfProjectItems.append(video.projectItem)
 	print("Creating sequence: '" + sequence_name +"'")
 	project.createNewSequenceFromClips(sequence_name, arrayOfProjectItems)
+	# Move sequence item into bin
+	bin_item = None
+	seq_item = None
+	for i in range(project.rootItem.children.numItems):
+		item = project.rootItem.children[i]
+		if item.name == SEQUENCES_FOLDER_NAME:
+			bin_item = item
+		if item.name == sequence_name:
+			seq_item = item
+		if bin_item != None and seq_item != None:
+			print("Moving new sequence into premiere bin...")
+			seq_item.moveBin(bin_item)
 
 def move_sketches_audio_down_a_layer(sequence):
 	print("Moving sketches audio down a layer...")
@@ -153,7 +217,8 @@ def remove_temp_video_track(qe_project):
 
 def insert_sketches_loop_music(project):
 	print("Insert sketches loop music...")
-	music_item = get_music(project)
+	#music_item = get_music(project)
+	music_item = find_item_in_folder(project, AUDIO_FOLDER_NAME, SKETCH_MUSIC_ITEM_NAME)
 	print("Music_item: " + music_item.name)
 	sequence = project.activeSequence
 	video_track = sequence.videoTracks[0]
@@ -174,7 +239,7 @@ def insert_sketches_loop_music(project):
 	out_points = [item for item in out_points if item not in common_elements]
 	# Iterate through both lists
 	for in_point, out_point in zip(in_points, out_points):
-		print(f"In: {in_point} - Out: {out_point}")
+		#print(f"In: {in_point} - Out: {out_point}")
 		duration = (out_point - in_point) / TICKS_PER_SECONDS
 		music_item.setInPoint(0, 2) # 2 = Audio only
 		music_item.setOutPoint(duration, 2) # 2 = Audio only
@@ -182,26 +247,29 @@ def insert_sketches_loop_music(project):
 
 def add_transitions(qe_project):
 	print("Adding transitions...")
+	
+	# Get active sequence and relevant tracks
 	qe_sequence = qe_project.getActiveSequence()
-	qe_video_tracks = []
-	qe_audio_tracks = []
-	qe_video_tracks.append(qe_sequence.getVideoTrackAt(0)) # VoorComp Video
-	qe_audio_tracks.append(qe_sequence.getAudioTrackAt(0)) # VoorComp SFX + Sketches music
-	qe_audio_tracks.append(qe_sequence.getAudioTrackAt(1)) # Sketches SFX
+	qe_video_tracks = [qe_sequence.getVideoTrackAt(0)]
+	qe_audio_tracks = [qe_sequence.getAudioTrackAt(0), qe_sequence.getAudioTrackAt(1)]
+	
+	# Helper function to add transitions
+	def add_transition(track, transition_type, time_code):
+		print(f"Adding {transition_type} transitions...")
+		transition_function = qe_project.getVideoTransitionByName if transition_type == "video" else qe_project.getAudioTransitionByName
+		transition_name = "Cross Dissolve" if transition_type == "video" else "Exponential Fade"
+		transition_to_apply = transition_function(transition_name)
+		
+		num_items_offset = -2 if transition_type == "video" else -1
+		for j in range(track.numItems + num_items_offset):
+			item = track.getItemAt(j)
+			item.addTransition(transition_to_apply, False, time_code)
+	
+	# Add transitions to video and audio tracks
 	for qe_video_track in qe_video_tracks:
-		print("Adding video transitions...")
-		for j in range(qe_video_track.numItems - 2):
-			item = qe_video_track.getItemAt(j)
-			video_transition_to_apply = qe_project.getVideoTransitionByName("Cross Dissolve")
-			time = '00;00;10;00'
-			item.addTransition(video_transition_to_apply, False, time)
+		add_transition(qe_video_track, "video", '00;00;01;00')
 	for qe_audio_track in qe_audio_tracks:
-		print("Adding audio transitions...")
-		for j in range(qe_audio_track.numItems - 2):
-			item = qe_audio_track.getItemAt(j)
-			audio_transition_to_apply = qe_project.getAudioTransitionByName("Exponential Fade")
-			time = '00;00;10;00'
-			item.addTransition(audio_transition_to_apply, False, time)
+		add_transition(qe_audio_track, "audio", '00;00;01;00')
 
 def remove_empty_tracks(qe_project):
 	print("Removing empty tracks from sequence...")
